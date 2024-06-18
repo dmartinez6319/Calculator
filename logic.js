a30b8232aecc359f973188c13442df388700634b
Daniel Martinez <dmartinez6319@gmail.com>
2024-06-18 19:40:36 -0400

    basic calculator operations added

--- logic.js
+++ logic.js
@@ -2,6 +2,9 @@ let operation = false;
 let decimal = false
 let initialNum = 0;
 let modifyNum = 0;
+let initalPart = false;
+let modifiyPart = false;
+let sliced;
 
 function addNum() {
     return initialNum += modifyNum;
@@ -16,7 +19,13 @@ function multNum() {
 }
 
 function divNum() {
+    if (modifyNum === 0) {
+        displayBox.innerHTML = "ERR";
+        return false
+    }
+    
     return initialNum /= modifyNum
+
 }
 
 const displayBox = document.getElementById("display");
@@ -30,13 +39,15 @@ function clearOut() {
     decimal = false;
     initialNum = 0;
     modifyNum = 0;
+    initalPart = false;
+    modifiyPart = false;
 }
 
 const numButtons = document.querySelectorAll(".num-button");
 numButtons.forEach(function(v){
     v.addEventListener("click",function(){
         let num = Number(v.innerHTML);
-        if (num === 0 && Number(displayBox.innerHTML) === 0){
+        if (num === 0 && Number(displayBox.innerHTML) === 0 && !decimal){
             return;
         }
         if (Number(displayBox.innerHTML) === 0 && !decimal) {
@@ -47,7 +58,50 @@ numButtons.forEach(function(v){
 })
 
 function doOperation(){
+    let result;
+    console.log(operation);
+    switch (operation) {
+        case "+":
+            result = addNum();
+            break;
+        case "-":
+            result = subNum();
+            break;
+        case "*":
+            result = multNum();
+            break;
+        case "/":
+            result = divNum();
+            break;
+    }
+    
+    displayBox.innerHTML = initialNum;
+    operation = false;
+    modifiyPart = false;
+    if (initialNum % 1 !== 0){
+        decimal = false;
+    }
+    console.log("Result set to: " + initialNum);
+    console.log("Operation set to: " + typeof(operation));
+}
 
+function setInitalNumber(op){
+    if ((!decimal || op !== "=") && !initalPart) {
+        initalPart = true;
+        initialNum = Number(displayBox.innerHTML);
+        operation = op;
+        sliced = displayBox.innerHTML.length;
+        console.log("Initial Number set: " + initialNum);
+        
+    }
+}
+
+function setModifyNumber(){
+    if (initalPart){
+        modifyNum = Number(displayBox.innerHTML.slice(sliced+3));
+        console.log("Modified number is: " + modifyNum);
+        modifiyPart = true;
+    }
 }
 
 function checkOperation(op) {
@@ -56,18 +110,21 @@ function checkOperation(op) {
         return;
     }
 
-    if (modifyNum > 0) {
-        doOperation();
-    }
+    console.log("Operation is: "+ typeof(operation))
     if (op !== "." && op !== "=" && typeof(operation) !== "boolean") {
+        console.log("i delted")
         let x = displayBox.innerHTML.slice(0,displayBox.innerHTML.length-3);
         displayBox.innerHTML = x;
     }
-    if (!decimal || op !== "=" && modifyNum == 0) {
-        initialNum = Number(displayBox.innerHTML);
-    }
 
-    operation = op;
+    setInitalNumber(op);
+    if (op !== "="){
+        console.log("Changed operation: " + op);
+        operation = op;
+    }
+    if (initalPart){
+        decimal = false;
+    }
     switch (op) {
         case "+":
             displayNum(" + ");
@@ -85,7 +142,8 @@ function checkOperation(op) {
             clearOut();
             break;
         case "=":
-
+            setModifyNumber();
+            doOperation(op);
             break;
         case ".":
             if (decimal) {
@@ -96,6 +154,7 @@ function checkOperation(op) {
             displayNum(".");
             break;
     }
+    // operation = op;
 }
 
 const opButtons = document.querySelectorAll(".op-button");
